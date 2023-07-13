bdf120e8ebaf0774d1cf16a2a9184327eeb6315d
skylionAkash <akashraj608@gmail.com>
2023-07-13 23:16:40 +0530

    changes

--- akash-next-app/src/app/page.js
+++ akash-next-app/src/app/page.js
@@ -1,20 +1,31 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <h1>Akash Singh</h1>
+//       <div className={styles.center}>
+//         <Image
+//           className={styles.logo}
+//           src="/next.svg"
+//           alt="Next.js Logo"
+//           width={180}
+//           height={37}
+//           priority
+//         />
+//       </div>
+//     </main>
+//   );
+// }
+
+import React from "react";
+
+const Page = () => {
   return (
-    <main className={styles.main}>
-      <h1>Akash Singh</h1>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-    </main>
+    <>
+      <h1>Hello Next JS</h1>
+    </>
   );
-}
+};
+export default Page;
