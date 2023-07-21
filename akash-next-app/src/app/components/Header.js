3857179c134495b87501492e2135969f9b8a8227
skylionAkash <akashraj608@gmail.com>
2023-07-21 23:49:37 +0530

    changes

---
+++ akash-next-app/src/app/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "@/app/styles/navbar.module.css";
+
+function Header() {
+  return <header></header>;
+}
+
+export default Header;
---
+++ akash-next-app/src/app/contact/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <h1>Contact page</h1>;
+}
+
+export default page;
