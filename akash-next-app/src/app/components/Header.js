f303da4a78c51a61ac6b9a9692e0c0a6f320ca66
skylionAkash <akashraj608@gmail.com>
2023-07-24 23:18:18 +0530

    changes

--- akash-next-app/src/app/components/Header.js
+++ akash-next-app/src/app/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "@/app/styles/navbar.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 function Header() {
   return (
     <header className={styles.main_header}>
       <ul className={styles.ul_list}>
+        <li>
+          <Link href="/">
+            <Image src="/next.svg" alt="next" width={150} height={100} />
+          </Link>
+        </li>
         <li>
           <Link href="/">Home</Link>
         </li>
--- akash-next-app/src/app/contact/page.js
+++ akash-next-app/src/app/contact/page.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "../components/Header";
 
 function page() {
   return (
     <>
-      <Header />
       <h1>Contact page</h1>
     </>
   );
